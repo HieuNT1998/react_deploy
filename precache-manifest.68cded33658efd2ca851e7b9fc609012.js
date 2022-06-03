5143dc4eb8a4d8160e93c2a8aee7b19219bcd1dc
HieuNT1998 <trihieua2@gmail.com>
2022-06-04 02:49:45 +0700

    Updates

---
+++ precache-manifest.68cded33658efd2ca851e7b9fc609012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "998e1f7cb933a0c0583b",
+    "url": "/react_deploy/static/css/main.8ca27d91.chunk.css"
+  },
+  {
+    "revision": "998e1f7cb933a0c0583b",
+    "url": "/react_deploy/static/js/main.27c04c1c.chunk.js"
+  },
+  {
+    "revision": "51aebe9d32ab23188641",
+    "url": "/react_deploy/static/js/runtime~main.f759f2d8.js"
+  },
+  {
+    "revision": "ef72a72af5b5b1ac0e44",
+    "url": "/react_deploy/static/js/2.b6a0e90a.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/react_deploy/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react_deploy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "873342ee708c8544f72a6bb877f9ec55",
+    "url": "/react_deploy/index.html"
+  }
+];
